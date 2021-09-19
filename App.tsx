e360c615293a228d81e90399d8f51921549ad1ee
fabio <fabio_santos_2001@outlook.com>
2021-09-19 15:16:21 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Dashboard } from "./src/pages/Dashboard";
+
+export default function App() {
+	return <Dashboard />;
+}
---
+++ src/pages/global/styles/theme.ts
@@ -0,0 +1,19 @@
+export default {
+	colors: {
+		primary: "#5636d3",
+
+		secondary: "#ff872c",
+		secondaryLight: "rgba(255, 135, 44, 0.3)",
+
+		success: "#12A454",
+		successLight: "rgba(18, 164, 84 , 0.5)",
+
+		attention: "#E83F5B",
+		attentionLight: "rgba(232, 63, 91, 0.5)",
+
+		shape: "#FFF",
+		title: "#363F5F",
+		text: "#969CB2",
+		background: "#F0F2F5",
+	},
+};
