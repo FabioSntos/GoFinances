33d4bea6ca0ed4808fa60a43608d3e1102d6c640
fabio <fabio_santos_2001@outlook.com>
2021-09-20 15:41:58 -0300

    Header

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,36 @@
 import React from "react";
 
-import { Container, Title } from "./styles";
+import {
+	Container,
+	Header,
+	UserContainer,
+	UserInfo,
+	Photo,
+	User,
+	UserGreeting,
+	UserName,
+	PowerButton,
+} from "./styles";
 
 export const Dashboard = () => {
 	return (
 		<Container>
-			<Title>Dashboard</Title>
+			<Header>
+				<UserContainer>
+					<UserInfo>
+						<Photo
+							source={{
+								uri: "https://avatars.githubusercontent.com/u/57735438?v=4",
+							}}
+						/>
+						<User>
+							<UserGreeting>Olá,</UserGreeting>
+							<UserName>Fábio</UserName>
+						</User>
+					</UserInfo>
+				</UserContainer>
+				<PowerButton name="power" />
+			</Header>
 		</Container>
 	);
 };
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,15 +1,51 @@
 import styled from "styled-components/native";
+import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
+
+import { Feather } from "@expo/vector-icons";
 
 export const Container = styled.View`
 	flex: 1;
-	justify-content: center;
-	align-items: center;
-
 	background: ${({ theme }) => theme.colors.background};
 `;
 
-export const Title = styled.Text`
+export const Header = styled.View`
+	width: 100%;
+	height: ${RFPercentage(42)}px;
+
+	background: ${({ theme }) => theme.colors.primary};
+	justify-content: center;
+	align-items: center;
+	flex-direction: row;
+`;
+export const UserContainer = styled.View`
+	width: 80%;
+	justify-content: space-between;
+`;
+export const UserInfo = styled.View`
+	flex-direction: row;
+	align-items: center;
+`;
+export const Photo = styled.Image`
+	width: ${RFValue(48)}px;
+	height: ${RFValue(48)}px;
+
+	border-radius: 10px;
+`;
+export const User = styled.View`
+	margin-left: 18px;
+`;
+export const UserGreeting = styled.Text`
+	color: ${({ theme }) => theme.colors.shape};
+	font-size: ${RFValue(18)}px;
 	font-family: ${({ theme }) => theme.fonts.regular};
-	color: ${({ theme }) => theme.colors.title};
-	font-size: 24px;
+`;
+export const UserName = styled.Text`
+	color: ${({ theme }) => theme.colors.shape};
+	font-size: ${RFValue(18)}px;
+	font-family: ${({ theme }) => theme.fonts.bold};
+`;
+
+export const PowerButton = styled(Feather)`
+	color: ${({ theme }) => theme.colors.secondary};
+	font-size: ${RFValue(30)}px;
 `;
